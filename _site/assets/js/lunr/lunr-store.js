77e911dca79e667d32843e5fbaba2e3fcb7a4592
so-woon-1221 <janus7359@gmail.com>
2021-02-06 01:44:01 +0900

    쇼핑몰 포스트 기본페이지 구성 완료 이제 추가되는 내용은 하나씩 추가해나갈 것임

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -6,7 +6,7 @@ var store = [{
         "teaser": null
       },{
         "title": "쇼핑몰 소개",
-        "excerpt":"1. 계기   React와 Spring을 교재를 활용하여 2달정도 공부했다.   두 가지 교재 모두 최종적으로는 게시판을 만드는것이 목표였다.   ","categories": ["Shopping mall"],
+        "excerpt":"1. 계기 React와 Spring을 교재를 활용하여 2달정도 공부했다. 두 가지 교재 모두 최종적으로는 게시판을 만드는것이 목표였다. React를 공부할때는 프론트엔드는 React, 백엔드는 Koa를 활용했다. Spring을 공부할때는 프론트엔드는 mustache, 백엔드는 Spring Boot를 활용했다. 결론적으로 React와 Spring을 모두 활용해서 프로젝트를 진행하고 싶은 생각이 들었고 데이터베이스로는 여태껏 접해왔던 SQL같은 RDB보다 MongoDB를 사용해보고싶었다. 이런저런 고민을...","categories": ["Shopping mall"],
         "tags": ["Shopping mall","쇼핑몰","프로젝트"],
         "url": "https://so-woon-1221.github.io/shopping%20mall/%EC%87%BC%ED%95%91%EB%AA%B0%EA%B0%9C%EC%9A%94/",
         "teaser": null
