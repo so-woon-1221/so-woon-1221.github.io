b7af34fcc9a30fdb15fc9132ff483ee257b28488
so-woon-1221 <janus7359@gmail.com>
2021-02-15 19:47:15 +0900

    음악 포스트 추가

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -17,6 +17,12 @@ var store = [{
         "url": "https://so-woon-1221.github.io/shoppingmall/insert",
         "teaser": null
       },{
+        "title": "나이값",
+        "excerpt":"나이값(rpod. Khalibeats) 블로그에 하나 정도는 다른 주제의 포스트를 하고 싶어서 평상시 취미로 만드는 자작곡 중 하나를 올립니다. 사실 실력이 나쁘고 장비도 나쁘기 때문에 음질도 별로지만 꾸준히 음악을 하는 이유는 나를 표현하는데 이만한 것이 없다고 생각했기 떄문이다. 들어주는 사람은 없지만 그래도 스트레스 해소나 생각 정리에는 굉장히 도움이 된다. 이번에 만든건 나이값이란...","categories": ["음악"],
+        "tags": ["음악","힙합"],
+        "url": "https://so-woon-1221.github.io/%EC%9D%8C%EC%95%85/%EB%85%B8%EB%9E%98%ED%85%8C%EC%8A%A4%ED%8A%B8/",
+        "teaser": null
+      },{
         "title": "쇼핑몰 - 서버 데이터 전송",
         "excerpt":"1. 개요 axios를 사용하여 백엔드와 통신한다. 그러나 백엔드는 Spring으로 구성되어 8080포트를 사용하고 프론트엔드는 3000포트를 사용해 CORS 이슈가 발생하게 된다. 따라서 proxy 설정을 통 이를 해결한다. 2. 상세 설명 package.json ... \"proxy\": \"http://localhost:8080/\" ... 프록시 proxy 설정을 spirng의 포트인 8080으로 설정한다. /lib/api/client.js import axios from 'axios' const client = axios.create(); export...","categories": ["Shopping mall"],
         "tags": ["Shopping mall","쇼핑몰","프로젝트"],
