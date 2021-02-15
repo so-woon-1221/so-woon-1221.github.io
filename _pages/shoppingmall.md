15a4d12220adb781bd76a38da55d5c058b83c2ff
so-woon-1221 <janus7359@gmail.com>
2021-02-15 21:24:33 +0900

    상세페이지 포스트 등록

--- _pages/shoppingmall.md
+++ _pages/shoppingmall.md
@@ -5,14 +5,9 @@ layout: single
 author_profile: true
 ---
 
-   
-   
 쇼핑몰을 제작하며 작성한 포스트들을 모아놓았습니다.
 
-   
-   
-   
-
-1. [  쇼핑몰개요](/shoppingmallindex)
+1. [ 쇼핑몰개요](/shoppingmallindex)
 2. [ 쇼핑몰 - 서버와 데이터 교환](/shoppingmall/axios)
 3. [ 쇼핑몰 - 상품 등록](/shoppingmall/insert)
+4. [ 쇼핑몰 - 제품 상세 페이지](/shoppingmall/infopage)
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -12,7 +12,7 @@ var store = [{
         "teaser": null
       },{
         "title": "쇼핑몰 - 상품등록",
-        "excerpt":"1. 구현 화면 간단한 화면 구성이다. 제일 먼저 상품의 이름을 입력할 수 있다. 그 아래에는 Quill Editor를 활용하여 상세정보를 입력할 수 있다. 오른쪽 하단엔 가격정보를 입력할 수 있는 칸이 있으며 Number Format을 활용해 자동으로 세자리마다 “,” 삽입, “원” 삽입 기능을 구현했다. 상품의 대표 이미지는 추후 메인화면에 나타나질 thumbnail이다. 상품 검색에...","categories": ["Shopping mall"],
+        "excerpt":"1. 화면 구성 간단한 화면 구성이다. 제일 먼저 상품의 이름을 입력할 수 있다. 그 아래에는 Quill Editor를 활용하여 상세정보를 입력할 수 있다. 오른쪽 하단엔 가격정보를 입력할 수 있는 칸이 있으며 Number Format을 활용해 자동으로 세자리마다 “,” 삽입, “원” 삽입 기능을 구현했다. 상품의 대표 이미지는 추후 메인화면에 나타나질 thumbnail이다. 상품 검색에...","categories": ["Shopping mall"],
         "tags": ["Shopping mall","쇼핑몰","프로젝트"],
         "url": "https://so-woon-1221.github.io/shoppingmall/insert",
         "teaser": null
@@ -21,6 +21,12 @@ var store = [{
         "excerpt":"나이값(rpod. Khalibeats) 블로그에 하나 정도는 다른 주제의 포스트를 하고 싶어서 평상시 취미로 만드는 자작곡 중 하나를 올립니다. 사실 실력이 나쁘고 장비도 나쁘기 때문에 음질도 별로지만 꾸준히 음악을 하는 이유는 나를 표현하는데 이만한 것이 없다고 생각했기 떄문이다. 들어주는 사람은 없지만 그래도 스트레스 해소나 생각 정리에는 굉장히 도움이 된다. 이번에 만든건 나이값이란...","categories": ["음악"],
         "tags": ["음악","힙합"],
         "url": "https://so-woon-1221.github.io/%EC%9D%8C%EC%95%85/%EB%85%B8%EB%9E%98%ED%85%8C%EC%8A%A4%ED%8A%B8/",
+        "teaser": null
+      },{
+        "title": "쇼핑몰 - 제품 상세 페이지",
+        "excerpt":"1. 화면 구성 기본적인 화면 구성이다. 상단에는 헤더가 존재하며 미완성된 상태이긴 하지만 메인페이지로 이동 가능한 로고와 카테고리 메뉴, 검색창, 로그인 버튼이 있다. 하단에는 대표이미지로 설정한 이미지와 상품 이름, 가격, 태그가 표시된다. 구매와 장바구니 버튼이 존재하 그 하단엔 상세설명이 나오게 된다. 2. 상세 설명 /modules/item.js import { createAction, handleActions } from...","categories": ["Shopping mall"],
+        "tags": ["Shopping mall","쇼핑몰","프로젝트"],
+        "url": "https://so-woon-1221.github.io/shoppingmall/infopage",
         "teaser": null
       },{
         "title": "쇼핑몰 - 서버 데이터 전송",
